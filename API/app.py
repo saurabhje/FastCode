5efb8a91b29dac409cf6599c5f76391bd5b8df46
saurabhje <work.saurabh@outlook.com>
2024-03-31 14:40:24 +0530

    Database

--- API/app.py
+++ API/app.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return {
-        'hello' : 'Nigger'
-    }
+app.config['MYSQL_USER'] = 'sql6695518'
+app.config['MYSQL_PASSWORD'] = 'AirLJ7ew6x'
+app.config['MYSQL_HOST'] = 'sql6.freemysqlhosting.net' 
+app.config['MYSQL_DB'] = 'sql6695518'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    # cur.execute('''CREATE TABLE Example (id INT, name VARCHAR(40))''')
+    # cur.execute('''INSERT INTO Example VALUES (1, 'Anthony')''')
+    # cur.execute('''INSERT INTO Example VALUES(2, 'Billy')''')
+    # mysql.connection.commit()
+    cur.execute('''SELECT * FROM Example''')
+    results = cur.fetchall()
+    return results[0]['name']
