f05f89daa43138d4164bb763abe89982a68ecb4f
saurabhje <work.saurabh@outlook.com>
2024-04-05 21:36:22 +0530

    Theming

--- Client/components/typingGame.tsx
+++ Client/components/typingGame.tsx
@@ -14,7 +14,7 @@ export default function TypingGame() {
       <div className="py-4 px-4 lg:px-20 flex flex-col items-center">
         <div className="self-end flex items-center gap-1.5">
           <label htmlFor="moder" className="text-base">Text</label>
-          <Switch id="moder" className="data-[state=checked]:bg-foreground data-[state=unchecked]:bg-foreground" onClick={handleMode}/>
+          <Switch id="moder" className="data-[state=unchecked]:bg-foreground" onClick={handleMode}/>
           <label htmlFor="moder" className="text-base">Code</label>
         </div>
         {mode == 'text' ? <RandPhrase /> : <RandCode />}
