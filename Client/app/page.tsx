e0efd91c1d4ce64d461c0ec8cacf8fc3516470f6
saurabhje <work.saurabh@outlook.com>
2025-07-13 13:24:53 +0530

    Fix footer

--- Client/app/page.tsx
+++ Client/app/page.tsx
@@ -15,10 +15,6 @@ export default async function home() {
   return (
     <div className='px-4 lg:px-20 flex flex-col'>
       <ScrollToTypingGame present={present}/>
-      <footer className='mt-auto pb-2 text-zinc-600 flex justify-center gap-8'>
-        <a className='hover:text-primary hover:underline' href='https://saurabh.codes'></a>
-        <a className='hover:text-primary hover:underline' href='https://github.com/saurabhje/fastcode'>Github</a>
-      </footer>
     </div>
   )
 }
